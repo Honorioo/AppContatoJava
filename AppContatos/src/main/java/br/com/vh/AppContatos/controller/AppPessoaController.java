c7dce3769d75a3b283ffa3df53f7545f27ebe3bf
Honorioo <viniciusgomeshonorio@hotmail.com>
2025-02-27 13:19:31 -0300

    Criando Cors Global

--- AppContatos/src/main/java/br/com/vh/AppContatos/controller/AppPessoaController.java
+++ AppContatos/src/main/java/br/com/vh/AppContatos/controller/AppPessoaController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,10 +20,10 @@ import br.com.vh.AppContatos.dto.MalaDiretaDto;
 import br.com.vh.AppContatos.model.Pessoa;
 import br.com.vh.AppContatos.service.PessoaService;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("api/pessoa")
-@CrossOrigin(origins = "http://localhost:4200") 
 public class AppPessoaController {
 
 	@Autowired
@@ -68,7 +67,7 @@ public class AppPessoaController {
 		}
 	}
 	
-
+	@Transactional
 	@Operation(summary = "Atualiza Pessoa cadastrada")
 	@PutMapping("/{id}")
 	public ResponseEntity<MalaDiretaDto> updateContact(@PathVariable Long id, @RequestBody Pessoa pessoa) {
